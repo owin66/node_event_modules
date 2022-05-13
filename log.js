ae7f9f1a69739e3f71c88a8f1e59f1810f117e09
owin66 <78900270+owin66@users.noreply.github.com>
2022-05-13 19:33:07 +0300

    module_util

--- log.js
+++ log.js
@@ -1,12 +1,16 @@
 const EventEmitter = require('events')
 
-const emitter = new EventEmitter()
+/*const emitter = new EventEmitter()*/
+const util = require('util')
 
-class Logger extends EventEmitter {
+class Logger {
 
     log = (msg) => {
         console.log(msg)
         this.emit('some_event', {id: 1, text: "event text"})
     }
 }
+
+util.inherits(Logger, EventEmitter)
+
 module.exports = Logger
